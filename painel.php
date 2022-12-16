2be6a62c59e8a82b23615c3ce466da30ef4ede58
rafaelcolaco <rafaelcolaco15@gmail.com>
2022-12-16 15:50:24 -0300

    up

--- painel.php
+++ painel.php
@@ -1,4 +1,27 @@
 <?php
 include('verifica_login.php');
 
-print_r($_SESSION);exit;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <title>Painel</title>
+</head>
+<body>
+ <div>   
+    <nav class="navbar " style="background-color: #00FF00;">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Site Escolar</span>
+  </div>
+</nav>
+</div>
+    
+</body>
+</html>
